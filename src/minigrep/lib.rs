42f6215d42cab3f13faca16225b935f8a5bd528d
troykings <patrick.kings@live.com>
2023-03-14 19:28:01 +0300

    add tests to minigrep

--- src/minigrep/lib.rs
+++ src/minigrep/lib.rs
@@ -1,15 +1,24 @@
 pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
     let contents = std::fs::read_to_string(config.file_path)?;
 
-    println!("With text: \n {contents}");
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     // This is the idiomatic way of indicating that we are calling the run() function for its side effects only. i.e It doesn't return a value we need.
     Ok(())
 }
 
 pub struct Config {
-   pub query: String,
-   pub file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -22,6 +31,79 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        return Ok(Config { query, file_path });
+let ignore_case = std::env::var("IGNORE_CASE").is_ok();
+
+        return Ok(Config { query, file_path , ignore_case});
+    }
+}
+
+// 'a is an explicit lifetime
+// lifetime parameters specify which argument lifetime is connected to the lifetime of the return value. i.e the returned vector should contain string slices that refernce slices of the argument 'contents' rather than the argument 'query'
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insesitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "safe, fast, productive.";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            crate::lib::search(query, contents)
+        );
     }
 }
--- src/minigrep/main.rs
+++ src/minigrep/main.rs
@@ -1,11 +1,11 @@
 mod lib;
 
 //
-use lib::*;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = lib::Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         std::process::exit(1);
     });
